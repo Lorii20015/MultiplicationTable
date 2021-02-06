891621d3f03a2824eb4cef247bf26478bd584174
Laura Bilíkova <bilikovalaura@bilikova.onmicrosoft.com>
2021-02-06 18:48:17 +0100

    Yuss

---
+++ ListOfEquasions/ListOfEquasions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ListOfEquasions
+{
+    class ListOfEquasions
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            int z = x * y;
+            while (x < 11 && y < 11)
+            {
+                y = ++y;
+                if (y % 11 == 0)
+                {
+                    x = ++x;
+                    y = y - 10;
+                    if (x == 11)
+                    {
+                        break;
+                    }
+                }
+                z = x * y;
+                Console.WriteLine(x + "x" + y + "=" + z);
+            }
+        }
+    }
+}
